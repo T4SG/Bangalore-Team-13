29ea389d5112e409090f52f0856f4545615f8efb
Keshav-Pandey <keshav25@outlook.com>
2015-07-25 18:38:38 +0530

    Sample Profile

--- DreamSchool/js/Data.js
+++ DreamSchool/js/Data.js
@@ -1,5 +1,6 @@
 ﻿var mentors = {
     "mentors": [
-    { "id": "M001", "name": "Anish Narang", "skills": "Communication:,Tests" }
+    { "id": "M001", "name": "Anish Narang", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" }
+
     ]
 };
