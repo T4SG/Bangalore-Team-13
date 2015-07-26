340eb97800b94d02ea7721e5a39b9aecddee96cd
Thumu <harishthumu3107@gmail.com>
2015-07-25 23:17:16 -0700

    mentor authentication
    mentor authentiction

--- mentorphp.php
+++ mentorphp.php
@@ -1,27 +1,46 @@
 <?php
 
-$name=$_POST['firstname'];
-$middlename=$_POST['middlename'];
-$lastname=$_POST['lastname'];
-$email=$_POST['email'];
-$gender="Male";
-$language=$_POST['language'];
-$tel=$_POST['tel'];
-$mobile=$_POST['mobile'];
-$address=$_POST['add'];
-$ptc=$_POST['ptc'];
-$pwd=$_POST['pwd'];
+$name=$_POST['name'];
 
-$con=mysqli_connect("localhost","root","","dfs");
+$middlelname=$_POST['middlename'];
+
+$lastname=$_POST['lastname'];
+$middlelname=$_POST['middlename'];
+$email=$_POST['email'];
+$tel=$_POST['tel'];
+
+$mobile=$_POST['mobile'];
+
+$address=$_POST['add'];
+$postalcode=$POST['ptc'];
+
+$username=$POST['username'];
+
+
+
+$con=mysql_connect("localhost","root","");
+
 
 if(!$con)
-	echo "Connectivity Error";
 
-$query="insert into mentor values ('$name','$middlename','$lastname','$gender','$language','$email','$tel','$mobile','$address','$ptc','$email','$pwd')";
-if(mysqli_query($con,$query))
-	echo("Success");
-else
-	echo(mysqli_error($con));
+echo "error";
+
+
+$query=sprintf("INSERT INTO dfs.mentor VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s')");
+
+
+
+mysql_query($query,$con);
+
+
+
+echo "successfully registered";
+
+
+
+
+
+
 ?>
 
 
