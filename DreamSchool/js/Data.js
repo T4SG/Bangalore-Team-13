2655b5ea25903e5fa58a334408a9c045cdceefc8
Keshav-Pandey <keshav25@outlook.com>
2015-07-26 07:24:42 +0530

    Match logic in htmlpage

--- DreamSchool/js/Data.js
+++ DreamSchool/js/Data.js
@@ -1,12 +1,12 @@
 ﻿var mentors = {
     "mentors": [
-    { "id": "M001", "name": "Anish Narang", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Learning Skills:Time Management,Revision Skills,Research Skills" },
-    { "id": "M002", "name": "Arvind Srikantan", "skills": "Personal Skills:Verbal Communication,Listening Skills;Leadership Skills:Organising Skills,Risk Management" },
-    { "id": "M003", "name": "Aditya Dalwani", "skills": "Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
-    { "id": "M004", "name": "Keshav Pandey", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
-    { "id": "M005", "name": "Madhura Sharma", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
-    { "id": "M006", "name": "Sanchit Narayan", "skills": "Leadership Skills:Organising Skills,Action Planning;Learning Skills:Time Management,Research Skills" },
-    { "id": "M007", "name": "Nishanth Gupta", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Learning Skills:Time Management,Revision Skills,Research Skills" },
-    { "id": "M008", "name": "Aishwarya Rai", "skills": "Personal Skills:Verbal Communication,Listening Skills" },
+    { "id": "M001", "name": "Anish Narang", "gender":"Male" ,"skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Learning Skills:Time Management,Revision Skills,Research Skills" },
+    { "id": "M002", "name": "Arvind Srikantan", "gender": "Male", "skills": "Personal Skills:Verbal Communication,Listening Skills;Leadership Skills:Organising Skills,Risk Management" },
+    { "id": "M003", "name": "Aditya Dalwani", "gender": "Male", "skills": "Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
+    { "id": "M004", "name": "Keshav Pandey", "gender": "Male", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
+    { "id": "M005", "name": "Madhura Sharma", "gender": "Female", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Leadership Skills:Organising Skills,Action Planning,Risk Management;Learning Skills:Time Management,Revision Skills,Research Skills" },
+    { "id": "M006", "name": "Sanchit Narayan", "gender": "Male", "skills": "Leadership Skills:Organising Skills,Action Planning;Learning Skills:Time Management,Research Skills" },
+    { "id": "M008", "name": "Aishwarya Rai", "gender": "Female", "skills": "Personal Skills:Verbal Communication,Listening Skills" },
+    { "id": "M007", "name": "Nishanth Gupta", "gender": "Male", "skills": "Personal Skills:Verbal Communication,Confidence Building,Listening Skills;Learning Skills:Time Management,Revision Skills,Research Skills" }
     ]
 };
